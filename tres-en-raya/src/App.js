1b2ef435a1b318cc49f75181afecba969ba5e544
Milton Rojas <155449701+maroj464@users.noreply.github.com>
2024-01-16 15:15:30 +0000

    new-modification

---
+++ tres-en-raya/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+import TableroC from './components/TableroC';
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+
+      
+      <TableroC />
+    </div>
+  );
+}
+
+export default App;
